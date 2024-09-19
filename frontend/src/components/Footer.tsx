c79e23005099d0db0d7e87f0644009bd2d1e0d77
SHUBHAM SINGH <106802656+TheShubham1008@users.noreply.github.com>
2024-09-20 01:23:26 +0530

    Update Footer.tsx

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,9 +5,9 @@ const Footer = () => {
         <span className="text-2xl md:text-3xl text-white font-semibold tracking-tight mb-4 md:mb-0">
           My Stay.com
         </span>
-{/*         <span className="text-xl md:text-3xl text-white font-semibold tracking-tight mb-4 md:mb-0">
+        <span className="text-xl md:text-3xl text-white font-semibold tracking-tight mb-4 md:mb-0">
           Made with ❤️ by Shubham Singh 🚀✨
-        </span> */}
+        </span>
         <span className="text-white font-medium tracking-tight flex gap-4">
           <p className="cursor-pointer transition duration-300 ease-in-out hover:text-gray-200">Privacy Policy</p>
           <p className="cursor-pointer transition duration-300 ease-in-out hover:text-gray-200">Terms of Service</p>
